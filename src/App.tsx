8c642799b3b2890ce1343f362daa95d0ef869f2b
ishagoyal <iamishagoyal@gmail.com>
2025-05-09 17:29:23 +0530

    Some css styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import PokemonList from "./components/PokemonList";
 import Details from "./components/Details";
 
-const App = () => (
-  <div className="flex gap-8 p-6 bg-gray-50 min-h-screen">
-    <PokemonList />
-    <Details />
-  </div>
-);
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
+        <div className="overflow-y-auto">
+          <PokemonList />
+        </div>
+        <div className="flex items-center justify-center">
+          <Details />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
